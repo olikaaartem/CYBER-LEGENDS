75cb30f02a4a24ee397b60ba2e53ae4633136bfc
olikaaartem <artemchuk.olga95@gmail.com>
2026-08-11 17:07:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function startMusic() {
 
    /* ВІЩУН КОДУС */
   SOUNDS.kodusStart.volume = 1;
-  SOUNDS.kodusStart.playbackRate = 1;
+  SOUNDS.kodusStart.playbackRate = 1,3;
 
   SOUNDS.bg.play().catch(() => {});
 

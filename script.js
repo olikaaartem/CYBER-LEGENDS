f5e93b2f4baae3ff2e9955c2717103fe1ee3f1c8
olikaaartem <artemchuk.olga95@gmail.com>
2026-08-12 15:14:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ const SOUNDS = {
   storm: new Audio("music/storm.mp3"),
      
  /* ОЗВУЧКА ВІЩУНА КОДУСА */
-  kodusStart: new Audio("audio codyc/did_kodys_start.m4a")
+  kodusStart: new Audio("audio_kodyc/did_kodys_start.m4a")
 
 };
 
